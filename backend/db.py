660a6eedc4a38420b58ffb99936c89e71c5fe49b
Fran <117905964+franrmueller@users.noreply.github.com>
2025-05-16 19:28:44 +0200

    db initialization function

--- backend/db.py
+++ backend/db.py
@@ -10,7 +10,7 @@ def sql_connect():
     try:
         connection = mysql.connector.connect(
             host=os.getenv("DB_HOST"),
-            port=os.getenv("DB_PORT"),
+            port=int(os.getenv("DB_PORT")),
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
             database=os.getenv("DB_NAME"),
